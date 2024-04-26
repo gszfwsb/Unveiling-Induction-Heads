cb76f2cef19f2e34f1e33d1cc716c16a94605857
gszfwsb <shaobowang1009@sjtu.edu.cn>
2024-04-26 18:01:43 +0800

    revise model B

--- RPE/model_B.py
+++ RPE/model_B.py
@@ -3,6 +3,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+
+
+
+class SimplifiedLayerNorm(nn.Module):
+    def __init__(self, dim=-1, eps=1e-7):
+        super(SimplifiedLayerNorm, self).__init__()
+        self.dim = dim
+        self.eps = eps
+    def forward(self, x):
+        norm = torch.norm(x, dim=self.dim, keepdim=True)
+        out = x / (norm + self.eps)
+        return out
+
 class TwoLayerTransformer(nn.Module):
     def __init__(self, 
                 vocab_size, 
@@ -13,48 +26,62 @@ class TwoLayerTransformer(nn.Module):
                 w_minus, 
                 a):
         super(TwoLayerTransformer, self).__init__()
-        self.L = seq_length
+        self.T = seq_length
         self.H = num_heads
         self.M = window_length
         # Assuming d_model is equal to input_dim
-        self.T = vocab_size
-        self.W = torch.Tensor(self.L,self.H)
-        self.A = torch.Tensor(self.H*self.T,self.H*self.T)
-        self.init(w_plus, w_minus, a)
-        # set to parameters
-        self.W = nn.Parameter(self.W) 
-        self.A = nn.Parameter(self.A)
-        self.norm = nn.LayerNorm(self.T) # norm for vocab_size
+        self.d = vocab_size
+        # layer 1: attention
+        self.W = torch.Tensor(self.T,self.H)
+        # layer 1: norm
+        self.norm = SimplifiedLayerNorm(dim=-1)
+        # layer mlp
+        self.alpha_list = torch.tensor([[int(i) for i in format(num, f'0{self.H+1}b')] for num in range(2**(self.H+1))])
+        self.C_alpha_list = nn.Parameter(torch.zeros(2**(self.H+1)))
+        self.S_alpha_list = torch.tensor([torch.nonzero(alpha, as_tuple=True)[0]] for alpha in self.alpha_list)
+        # layer 2: attention
+        self.a = a
+             
     def init(self, w_plus, w_minus, a):
         # for W
         nn.init.constant_(self.W, w_minus)
         self.W.diagonal()[:self.M].fill_(w_plus) 
-        # for A
-        nn.init.zeros_(self.A)
-        self.A.diagonal().fill_(a)
 
     def forward(self, x):
-        # x: (bs, L, S)
+        # x: (bs, T, d)
         # First layer
+        # concatenate an all-zero vecgor as the last row of X
+        x = torch.cat([x, torch.zeros((x.size(0), 1, x.size(2)))], dim=1) # (bs, T+1, d)
         V = []
         for i in range(self.H):
-            W_h = torch.zeros((self.L, self.L)).to(x.device) # L*L
-            for j in range(self.L):
+            W_h = torch.zeros((self.T+1, self.T+1)).to(x.device) # L*L
+            for j in range(self.T):
                 torch.diagonal(W_h, -j).fill_(self.W[:, i][j])  # Set the (j+1)-th negative diagonal
             mask = torch.triu(torch.full_like(W_h, float('-inf')), diagonal=1)
             W_h = W_h + mask
             W_h = F.softmax(W_h, dim=-1)
-            attn_h = torch.matmul(W_h, x) # [L, L], [bs, L, S]
-            V.append(attn_h)
-        V.append(x) # [bs, L, S, (H+1)]
-        V = torch.stack(V) # [bs, L, S, (H+1)]
-        V = self.norm(V) # [bs, L, S, (H+1)]
-        W_2 = torch.matmul(torch.matmul(V[:,-1:,:self.H*self.T], self.A),
-                            V[:,:-1,:self.H*self.T].permute(0,2,1)) # [bs, 1, HS], [HS, HS], [bs, HS, L-1]
-        W_2 = F.softmax(W_2, dim=-1) # [bs, L, L-1]
-        Y = torch.matmul(W_2, V[:,:-1,self.H*self.T:]) # [bs, 1, L-1], [bs, L-1, S] -> [bs, 1, S]
-        Y = Y.squeeze(1) # [bs, S]
-        return Y
+            v_h = torch.matmul(W_h, x) # [T+1, T+1], [bs, T+1, d] -> [bs, T+1, d]
+            v_h_normalized = self.norm(v_h)
+            V.append(v_h_normalized)
+        V.append(x) # [bs, T+1, d, (H+1)]
+        V = torch.stack(V) # [bs, T+1, d, (H+1)]
+        V = self.norm(V) # [bs, T+1, d, (H+1)]
+
+    def polynomial_kernel(self, v_t, v_t_prime):
+        '''
+        v_t: [bs, d, (H+1)]
+        v_t_prime: [bs, d, (H+1)]
+        '''
+        # Initialize the kernel result
+        kernel_result = 0.0
+        
+        # For each binary vector alpha, compute the product term
+        for idx, (S_alpha, C_alpha) in enumerate(zip(self.S_alpha_list, self.C_alpha_list)):
+            product_term = torch.prod(torch.stack([torch.dot(v_t[...,h], v_t_prime[..., h]) for h in S_alpha]))
+            kernel_result += C_alpha**2 * product_term
+            
+        return kernel_result
+
 
 def test_model():
     # Parameters
@@ -74,4 +101,4 @@ def test_model():
     output = model(x)
     print(output)
 
-# test_model()
+test_model()
--- RPE/test.py
+++ RPE/test.py
@@ -1,4 +1,49 @@
-import numpuy
+import torch
 
-def print_pig():
-    ```generate prime number 
+def polynomial_kernel(v_t, v_t_prime, S_alpha_list, C_alpha_list):
+    '''
+    v_t: [bs, d, (H+1)]
+    v_t_prime: [bs, d, (H+1)]
+    '''
+    # Compute the dot product between v_t and v_t_prime
+    dot_product = torch.matmul(v_t.transpose(-1, -2), v_t_prime)
+    
+    # Compute the product term for each binary vector alpha
+    product_terms = torch.prod(torch.gather(dot_product, -1, S_alpha_list.unsqueeze(-1)), dim=-1)
+    
+    # Compute the kernel result using matrix operations
+    kernel_result = torch.matmul(C_alpha_list**2, product_terms)
+        
+    return kernel_result
+
+
+def polynomial_kernel_2(v_t, v_t_prime, S_alpha_list, C_alpha_list):
+    '''
+    v_t: [bs, d, (H+1)]
+    v_t_prime: [bs, d, (H+1)]
+    '''
+    # Initialize the kernel result
+    kernel_result = 0.0
+    
+    # For each binary vector alpha, compute the product term
+    for idx, (S_alpha, C_alpha) in enumerate(zip(S_alpha_list, C_alpha_list)):
+        product_term = torch.prod(torch.stack([torch.dot(v_t[...,h], v_t_prime[..., h]) for h in S_alpha]))
+        kernel_result += C_alpha**2 * product_term
+        
+    return kernel_result
+
+# Generate test samples
+bs = 2  # batch size
+d = 4   # dimensionality
+H = 3   # H value
+v_t = torch.randn(bs, d, H+1)
+v_t_prime = torch.randn(bs, d, H+1)
+S_alpha_list = torch.randint(low=0, high=H+1, size=(2**(H+1), H))
+C_alpha_list = torch.randn(2**(H+1))
+
+# Test the functions
+kernel_result_1 = polynomial_kernel(v_t, v_t_prime, S_alpha_list, C_alpha_list)
+kernel_result_2 = polynomial_kernel_2(v_t, v_t_prime, S_alpha_list, C_alpha_list)
+
+print(kernel_result_1.shape, kernel_result_2.shape)
+
