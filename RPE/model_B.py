fix gradient

RPE/model_B.py
@@ -26,6 +26,7 @@ class TwoLayerTransformer(nn.Module):
         self.T = seq_length
         self.H = num_heads
         self.M = window_length
+        assert self.H <= self.M, 'Number of heads should be less than or equal to window length'
         # Assuming d_model is equal to input_dim
         self.d = vocab_size
         # layer 1: attention
@@ -72,7 +73,8 @@ class TwoLayerTransformer(nn.Module):
         for idx, (C_alpha, S_alpha) in enumerate(zip(self.C_alpha_list, self.S_alpha_list)):
             if len(S_alpha) == 0:
                 continue
-            product_term = torch.prod(torch.stack([torch.einsum('bd,bd->b',v_t[...,h], v_t_prime[..., h]) for h in S_alpha]))
+            support_S = torch.stack([torch.einsum('bd,bd->b',v_t[...,h], v_t_prime[..., h]) for h in S_alpha]) # |S_alpha| * ([bs, d] [bs, d] -> [bs]) -> [|S_alpha|, bs]
+            product_term = torch.prod(support_S, dim=0) # [bs]
             kernel_result += C_alpha**2 * product_term
         return kernel_result
 
@@ -83,11 +85,10 @@ class TwoLayerTransformer(nn.Module):
         # print('X_tilde:', X_tilde.shape)
         V = []
         for i in range(self.H):
-            W_h = torch.zeros((self.T+1, self.T+1)).to(X_tilde.device) # [T+1, T+1]
+            # W_h = torch.zeros((self.T+1, self.T+1)).to(X_tilde.device) # [T+1, T+1]
+            W_h = torch.full((self.T+1, self.T+1), float('-inf'), device=X.device) # [T+1, T+1]
             for j in range(self.T):
                 torch.diagonal(W_h, -j).fill_(self.W[:, i][j])  # Set the (j+1)-th negative diagonal
-            mask = torch.triu(torch.full_like(W_h, float('-inf')), diagonal=1)
-            W_h = W_h + mask
             W_h = F.softmax(W_h, dim=-1)
             v_h = torch.matmul(W_h, X_tilde) # [T+1, T+1], [bs, T+1, d] -> [bs, T+1, d]
             v_h_normalized = self.norm(v_h)
@@ -97,7 +98,9 @@ class TwoLayerTransformer(nn.Module):
         V = torch.stack(V, -1) # [bs, T+1, d, (H+1)]
         kernel_prod = [] # [bs, T]
         for i in range(self.T):
-            kernel_prod.append(self.polynomial_kernel(V[:, -1], V[:, i])) # [bs, d, (H+1)], [bs, d, (H+1)] -> [bs]
-        kernel_prod = torch.tensor(kernel_prod).to(X.device) # [bs, T]
-        y = torch.matmul(F.softmax(self.a * kernel_prod, dim=-1), X) # [bs, T], [bs, T, d] -> [bs, d]
+            prod = self.polynomial_kernel(V[:, -1], V[:, i]) # [bs, d, (H+1)], [bs, d, (H+1)] -> [bs]
+            kernel_prod.append(prod) # [bs, T]
+        kernel_prod = torch.stack(kernel_prod, -1).to(X.device) # [bs, T]
+        kernel_prod = F.softmax(self.a * kernel_prod, dim=-1) # [bs, T]
+        y = torch.einsum('bt,btd->bd',kernel_prod, X) # [bs, T], [bs, T, d] -> [bs, d]
         return y

RPE/train_independently_model_B.py
@@ -81,10 +81,10 @@ def visualize_params(W, C_alpha, save_file_path, epoch=-1, phase=1):
 
 
 parser = argparse.ArgumentParser('train 2-layer disentangled Transformer')
-parser.add_argument('--vocab-size',type=int,default=10)
-parser.add_argument('--seq-length',type=int, default=20)
-parser.add_argument('--window-length',type=int, default=8)
-parser.add_argument('--n-heads',type=int, default=5)
+parser.add_argument('--vocab-size',type=int,default=2)
+parser.add_argument('--seq-length',type=int, default=10)
+parser.add_argument('--window-length',type=int, default=5)
+parser.add_argument('--n-heads',type=int, default=3)
 parser.add_argument('--lr1',type=float, default=0.8)
 parser.add_argument('--lr2',type=float, default=0.8)
 parser.add_argument('--batch-size',type=int, default=100000)
@@ -169,8 +169,11 @@ n_train, n_val = int(n_sample * 0.9), int(n_sample * 0.1)
 if os.path.exists(f'{data_path}/train_set.pt'):
     train_dataset = torch.load(f'{data_path}/train_set.pt')
     val_dataset = torch.load(f'{data_path}/val_set.pt')
-else:  
-    dataset = MarkovDataset(S, L, alpha, n_sample)
+else:
+    if dataset == 'Markov':
+        dataset = MarkovDataset(S, L, alpha, n_sample)
+    else:
+        dataset = NGramDataset(S, L, n, alpha, n_sample)
     # Split into train and validation sets
     train_dataset, val_dataset = torch.utils.data.random_split(dataset, [n_train, n_val])
     torch.save(train_dataset, f'{data_path}/train_set.pt')
@@ -209,8 +212,6 @@ for epoch in pbar:
         optimizer_1.step()
         # Update the learning rate
         # scheduler.step()
-        print("Gradient for a after backward:", model.a.grad)
-        print("Gradient for C_alpha_list after backward:", model.C_alpha_list.grad)
 
         pbar.set_description(f'Train loss:{loss.item():.10f}')
         wandb.log({'Train loss':loss.item()})    