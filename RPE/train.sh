ad99270edb7675672341b83f54347ffb75142280
gszfwsb <shaobowang1009@sjtu.edu.cn>
2024-05-21 16:49:45 +0800

    update

--- RPE/train.sh
+++ RPE/train.sh
@@ -1,12 +1,2 @@
 python3 train_simplified.py --vocab-size 3 --seq-length 100 --n-heads 5 --n-gram 4 --w-plus 2 --c-alpha 0.01 --device cuda:0 \
---n-epochs 1000 --n-epochs 500 --n-epochs 10 --lr 1 --lr 1 --lr 1 --train-cmd C --train-cmd W --train-cmd a --low-degree 3
-
-
-python3 train_simplified.py --vocab-size 3 --seq-length 100 --n-heads 5 --n-gram 4 --w-plus 2 --c-alpha 0.01 --device cuda:0 \
---n-epochs 1000 --n-epochs 500 --n-epochs 10000 --lr 1 --lr 1 --lr 1 --train-cmd C --train-cmd W --train-cmd a --low-degree 3
-
-
-
-
-python3 train_simplified.py --vocab-size 3 --seq-length 100 --n-heads 5 --n-gram 4 --w-plus 2 --c-alpha 0.01 --device cuda:0 \
---n-epochs 500 --n-epochs 500 --n-epochs 1 --lr 1 --lr 100 --lr 1 --train-cmd C --train-cmd W --train-cmd a --low-degree 3
+--n-epochs 500 --n-epochs 500 --n-epochs 500 --lr 1 --lr 100 --lr 10 --train-cmd C --train-cmd W --train-cmd a --low-degree 3
