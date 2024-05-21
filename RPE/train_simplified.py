cbd63e057e3c7b6a05637b68b0b258f795cb1c92
gszfwsb <shaobowang1009@sjtu.edu.cn>
2024-05-21 14:32:11 +0800

    udpate

--- RPE/train_simplified.py
+++ RPE/train_simplified.py
@@ -152,7 +152,7 @@ def main():
     parser.add_argument('--alpha',type=float,default=0.1)
     parser.add_argument('--n-epochs',type=int,action='append')
     parser.add_argument('--n-gram',type=int,default=3)
-    parser.add_argument('--low-degree',type=int,default=3)
+    parser.add_argument('--low-degree',type=int,default=-1)
     parser.add_argument('--train-cmd', action='append', type=str)
 
 
@@ -190,6 +190,7 @@ def main():
     method_args = f'{cmd_args}_parent{n-1}_n{n_sample}_L{L}_S{S}_H{H}_{lr_args}_opt{optim_method}_w+{w_plus}_w-{w_minus}_D{low_degree}_c_alpha_init{c_alpha_init}_a_init{a_init}_alpha{alpha}_n-epochs{n_epochs}'
     root_path = './data'
     save_file_path = osp.join(f'./results_paper', dataset, method_args)
+    os.makedirs(save_file_path, exist_ok=True)
     # Generate the TwoLayerCausalTransformer
     if low_degree != -1:
         model = SimplifiedTwoLayerTransformer(S, L, H, w_plus, w_minus, a_init, c_alpha_init, n-1, low_degree)
